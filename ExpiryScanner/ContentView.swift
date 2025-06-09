d4d7b7e87827650923771a211cbb2168dc973f72
Toniii67 <francopranara@gmail.com>
2025-06-09 08:53:03 +0700

    Initial Commit

---
+++ ExpiryScanner/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ExpiryScanner
+//
+//  Created by Franco Antonio Pranata on 09/06/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
